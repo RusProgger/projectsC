0c0478de40e2e54bae046ec1b495fe39e56b9a23
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2024-01-29 13:46:07 +0300

    Add files via upload

---
+++ character output/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+
+#define a 150
+#define f 5
+
+
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    putchar(a);
+
+    cout << a << endl;
+    putchar(f);
+
+    cout << f << endl;
+
+
+
+
+    _getch();
+    return 0;
+}
